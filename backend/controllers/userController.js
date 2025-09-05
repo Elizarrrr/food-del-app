efcc8b6a1437d0906a5582b183a4a7ccbca4e915
Elizarrrr <joyliz99.m@gmail.com>
2025-09-05 17:51:24 +0300

    user registration login authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,83 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // check if user's email exists in the database
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    // check if user's password matches the one in the database
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const createToken = (id) => {
+  // use user id to generate token and return the token
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+// register user
+const registerUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+    // checking if user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validating email format and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a stong password",
+      });
+    }
+
+    // encrypt (hashing) user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create new user
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    // save user to database
+    const user = await newUser.save();
+
+    // generate token and send it as a response
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { loginUser, registerUser };
