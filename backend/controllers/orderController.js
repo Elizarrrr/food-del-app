3defa94c305f7a272d9a0696b06d7d9941735e32
Elizabeth <joyliz99.m@gmail.com>
2026-05-13 22:54:48 +0300

    prepare app for production deployment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = process.env.FRONTEND_URL;
+  // const frontend_url = "http://localhost:5174";
 
   try {
     const newOrder = new orderModel({
